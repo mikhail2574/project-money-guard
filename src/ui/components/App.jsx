086cb86ea4e03caecc9cc37332dbc3b2896848de
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-13 16:34:55 +0300

    remove

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import LoginForm from './LoginForm/LoginForm';
 
 export const App = () => {
   return (
     <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
     >
-      {/* React homework template */}
-      <LoginForm />
+      React homework template
     </div>
   );
 };
