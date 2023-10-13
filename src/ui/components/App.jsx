7d9c9387d49aa6ea5318ddd61cf8b0ca3b5a7d97
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-13 16:29:04 +0300

    loginform styles

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import LoginForm from './LoginForm/LoginForm';
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      {/* React homework template */}
+      <LoginForm />
     </div>
   );
 };
