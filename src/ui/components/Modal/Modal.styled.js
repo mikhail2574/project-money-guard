92b7f674ea822e71b8759c1fd0628f979ea72d24
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-15 11:17:11 +0300

    add modal style

--- src/ui/components/Modal/Modal.styled.js
+++ src/ui/components/Modal/Modal.styled.js
@@ -4,24 +4,38 @@ import bd from 'ui/images/register/register-mobile.webp';
 
 export const StyledOverlay = styled.div`
   z-index: 300;
-  backdrop-filter: blur(5px);
   position: fixed;
   inset: 0;
   background-color: rgba(0, 0, 0, 0.6);
-  display: grid;
+  display: flex;
   place-content: center;
   @media screen and (max-width: 767px) {
     margin-top: 60px;
+    background-image: url(${bd});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-color: #1e0646;
+  }
+  @media screen and (min-width: 768px) {
+    display: grid;
+    background: rgba(34, 13, 91, 0.23);
+    backdrop-filter: blur(3.5px);
   }
 `;
+
 export const StyledContent = styled.div`
-  position: relative;
-  background-image: url(${bd});
-  background-repeat: no-repeat;
-  background-size: cover;
-  max-width: 540px;
-  border-radius: 8px;
-  box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
+  @media screen and (min-width: 768px) {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 540px;
+    background-image: url(${bd});
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 8px;
+    box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
+  }
 `;
 
 export const StyledButton = styled.span`
