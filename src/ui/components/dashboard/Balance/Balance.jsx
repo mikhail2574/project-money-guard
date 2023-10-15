7b1deead769d83a145c19022a5f63ae4bda9f2de
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-15 20:34:50 +0300

    add balans styles

--- src/ui/components/dashboard/Balance/Balance.jsx
+++ src/ui/components/dashboard/Balance/Balance.jsx
@@ -1,3 +1,54 @@
+// export const Balance = () => {
+//   return <p>"Your balance: 0 :("</p>;
+// };
+
+import { useMediaQuery } from 'react-responsive';
+import {
+  StyledBalance,
+  StyledBalanceBox,
+  StyledBalanceLabel,
+} from './Balance.styled';
+import { useSelector } from 'react-redux';
+import { selectName } from 'redux/registration/selectors';
+
 export const Balance = () => {
-  return <p>"Your balance: 0 :("</p>;
+  const balance = useSelector(selectName);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
+  return (
+    <div>
+      {isDesktop && (
+        <StyledBalanceBox>
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>
+
+            {balance ? <>{balance.balance}</> : ''}
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+      {isTablet && (
+        <StyledBalanceBox maxW="296px" padL="40px" borR="8px" marB="0px">
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>
+
+            {balance ? <>{balance.balance}</> : ''}
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+
+      {isMobile && (
+        <StyledBalanceBox maxW="440px" padL="40px" borR="8px" marB="32px">
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>
+
+            {balance ? <>{balance.balance}</> : ''}
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+    </div>
+  );
 };
