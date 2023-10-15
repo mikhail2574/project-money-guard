b63e7786de8db671625b4b5a7664552678046c95
D3mk4 <dmtvchenko@gmail.com>
2023-10-15 22:22:32 +0300

    update addTransaction styles
    need to change icon fo date picker and create logic for sending back ( add & change)

--- src/ui/components/home/modalWindows/AddTransactionForm/AddTransactionForm.jsx
+++ src/ui/components/home/modalWindows/AddTransactionForm/AddTransactionForm.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Select from 'react-select';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-// import { MdDateRange } from 'react-icons/md';
+import { MdDateRange } from 'react-icons/md';
 
 import {
+  StyledPicker,
+  CustomSelect,
+  customSelectStyles,
   StyledPlusMin,
   StyledSpanExpenses,
   StyledSpanIncome,
@@ -17,98 +18,11 @@ import {
   ButtonContainer,
 } from './AddTransactionForm.styled';
 import Switch from 'react-switch';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 import Button from 'ui/components/shared/Button';
 import { selectCategories } from 'redux/transactions/selectors';
 import { fetchCategories } from 'redux/transactions/operations';
 import { useMyContext } from 'context/useMyContext';
-
-const StyledPicker = styled(DatePicker)`
-  width: 182px;
-  border: none;
-  text-align: center;
-  outline: none;
-  background-color: transparent;
-  border-bottom: 1px solid var(--transp-40);
-  padding-bottom: 8px;
-  color: var(--white);
-  font-size: 18px;
-
-  .react-datepicker-wrapper {
-    @media screen and (max-width: 767px) {
-      width: 100%;
-    }
-  }
-
-  .react-datepicker__input-container {
-    border-bottom: 1px solid rgba(255, 255, 255, 0.4);
-    display: flex;
-  }
-
-  .date-icon {
-    position: absolute;
-    bottom: 12px;
-    right: 12px;
-    width: 24px;
-    height: 24px;
-    fill: var(--purple);
-
-    @media screen and (max-width: 767px) {
-      flex-direction: column;
-    }
-  }
-
-  @media screen and (max-width: 767px) {
-    width: 280px;
-    text-align: left;
-    padding-left: 20px;
-  }
-`;
-
-const CustomSelect = styled(Select)`
-  .my-select__control {
-    border-radius: 0;
-    border: none;
-    outline: none !important;
-    background-color: transparent;
-    border-bottom: 1px solid var(--transp-40);
-  }
-
-  .my-select__control input {
-    outline: none !important;
-  }
-
-  .my-select__indicator {
-    transform: ${props => (props.menuIsOpen ? 'rotate(180deg)' : 'none')};
-    display: ${props => (props.menuIsOpen ? 'block' : 'none')};
-  }
-`;
-
-const customSelectStyles = {
-  control: provided => ({
-    ...provided,
-    outline: 'none',
-    color: 'var(--white)',
-  }),
-  indicatorSeparator: () => ({
-    display: 'none',
-  }),
-  indicatorContainer: () => ({
-    display: 'none',
-  }),
-  menu: provided => ({
-    ...provided,
-    background: `linear-gradient(
-    210deg,
-    rgba(83, 61, 186, 1) 0%,
-    rgba(80, 48, 154, 1) 43.14%,
-    rgba(106, 70, 165, 1) 73.27%,
-    rgba(133, 93, 175, 0.9) 120.03%
-  )`,
-  }),
-};
 
 export const AddTransactionForm = () => {
   const [checked, setChecked] = useState(true);
@@ -179,8 +93,8 @@ export const AddTransactionForm = () => {
               shouldCloseOnSelect={true}
               selected={selectedDate}
               onChange={date => setSelectedDate(date)}
+              showIcon={MdDateRange}
             />
-            {/* <MdDateRange className="calendar-icon" /> */}
           </InputContainer>
           <StyledInput type="text" />
         </StyledDiv>
