de40621b27763491c100ca158ad0ae0d4167e2e9
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-16 12:29:34 +0300

    add container`s paddings

--- src/ui/styles/GlobalStyles.js
+++ src/ui/styles/GlobalStyles.js
@@ -89,15 +89,18 @@ const GlobalStyles = createGlobalStyle`
   .container {
   padding: 0 15px;
   margin: 0 auto;
+  outline: 1px solid red;
   @media screen and (min-width:320px) {
       max-width: 475px;
  }
   @media screen and (min-width:768px) {
       max-width: 768px;
+      padding: 0 32px;
  }
 
   @media screen and (min-width:1258px) {
       max-width: 1280px;
+      padding: 0 16px;
  }
 }
 `;
