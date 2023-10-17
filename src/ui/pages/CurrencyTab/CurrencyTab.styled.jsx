126715499e814bb309e9a5fcd4db6baf0d6cacce
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-17 15:03:34 +0300

    add CurrTab

---
+++ src/ui/pages/CurrencyTab/CurrencyTab.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledCarrencyWrap = styled.div`
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    position: absolute;
+    top: 0;
+  }
+`;
