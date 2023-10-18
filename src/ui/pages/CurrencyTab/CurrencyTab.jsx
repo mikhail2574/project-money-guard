ee74c3b6dbc7b49223bef2a4f0e8685c8eb3adc8
Disclo5ure <46845468m@gmail.com>
2023-10-18 10:01:32 +0300

    add logout modal to statistics and currency pages

--- src/ui/pages/CurrencyTab/CurrencyTab.jsx
+++ src/ui/pages/CurrencyTab/CurrencyTab.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Currency } from 'ui/components/dashboard/Currency/Currency';
 import { StyledCarrencyWrap } from './CurrencyTab.styled';
+import { useMyContext } from 'context/useMyContext';
+import { Modal } from 'ui/components/Modal/Modal';
+import { Logout } from 'ui/components/dashboard/Logout/Logout';
 
 const CurrencyTab = () => {
+  const { isOpen, typeModal } = useMyContext();
   return (
     <StyledCarrencyWrap>
       <Currency />
+      {isOpen ? <Modal>{typeModal === 'exit' ? <Logout /> : ''}</Modal> : null}
     </StyledCarrencyWrap>
   );
 };
