d98ec74db7cacf61585c1ebf8638da90ba08d468
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-10-24 15:12:31 +0200

    update 404

--- src/ui/pages/NotFoundPage/NotFoundPage.jsx
+++ src/ui/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,35 +4,16 @@ import { NotFoundPageSection, NotFoundPageStyled } from './NotFoundPage.styled';
 
 const NotFoundPage = () => {
   return (
-    <>
-      <html>
-        <head>
-          <meta charset="utf-8" />
-          <title>Single Page Apps for GitHub Pages</title>
-          <script type="text/javascript">
-            var pathSegmentsToKeep = 1; var l = window.location; l.replace(
-            l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
-            l.pathname .split('/') .slice(0, 1 + pathSegmentsToKeep) .join('/')
-            + '/?/' + l.pathname .slice(1) .split('/')
-            .slice(pathSegmentsToKeep) .join('/') .replace(/&/g, '~and~') +
-            (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-            l.hash
-          </script>
-        </head>
-        <body>
-          <NotFoundPageSection>
-            <NotFoundPageStyled className="text404">
-              <h3>Oops!</h3>
-              <h2>404</h2>
-              <h3>Page not found</h3>
-              <Link to={'/home'} className="link404">
-                Go Home
-              </Link>
-            </NotFoundPageStyled>
-          </NotFoundPageSection>
-        </body>
-      </html>
-    </>
+    <NotFoundPageSection>
+      <NotFoundPageStyled className="text404">
+        <h3>Oops!</h3>
+        <h2>404</h2>
+        <h3>Page not found</h3>
+        <Link to={'/home'} className="link404">
+          Go Home
+        </Link>
+      </NotFoundPageStyled>
+    </NotFoundPageSection>
   );
 };
 
